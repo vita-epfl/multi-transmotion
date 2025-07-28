83fe814f87762b0cd2ba7c028a8f5c7196f88ae9
Yang GAO <46134589+yanggao2000@users.noreply.github.com>
2025-07-28 17:49:17 +0200

    Update extract_NBA.py
    Fix the splitting issue, which doesn't affect the reported numbers (0.75/0.97) of our method. 
    Before: (0.7502/0.9705); After: (0.7528/0.9712)

--- extract_data/NBA/extract_NBA.py
+++ extract_data/NBA/extract_NBA.py
@@ -9,13 +9,24 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--split', type=str, default='train', help='train or val or test')
 args = parser.parse_args()
 split = args.split
-val_size = 1000
-scale_foot2meter = 0.3048
+scale_foot2meter = 0.3048 # convert foot to meter; to fairly compare with LED, we have a rescale in the line 114 of 'main/ft_NBA/evaluate.py'.
 
 ## extract partial data from train split as validation data
 if args.split == 'val':
     split = 'train'
 array_data = np.load(os.path.join(current_dir, 'raw','nba_'+split+'.npy'))
+
+
+if args.split == 'train':
+    array_data = array_data[:32500]
+elif args.split == 'test':
+    array_data = array_data[:12500] # extract the same samples as LED's dataloader_nba.py in https://drive.google.com/drive/folders/1NQffxbaEKeAa8pOjlmNqCdMzMmB68V04
+elif args.split == 'val':
+    array_data = array_data[32500:33500]
+    val_size = array_data.shape[0]
+
+else:
+    raise ValueError('split should be train, val or test')
 
 if not os.path.exists(current_dir+'/'+'output_csv'+'/'):
     os.makedirs(current_dir+'/'+'output_csv'+'/')
@@ -24,45 +35,13 @@ with open(os.path.join(current_dir,'output_csv','nba_'+args.split+'.csv'), 'w') 
     writer = csv.writer(result)
 
     num_batch = array_data.shape[0]
-    if args.split == 'test':
-        for batch in range(num_batch):
 
-            if batch % 100 == 0:
-                print(batch, '/', num_batch)
+    for batch in range(num_batch):
+        if batch % 100 == 0:
+            print(batch, '/', num_batch)
 
-            for frame in range(30):
-                for N in range(11):
-                    traj = (batch*30+frame, batch*11+N, array_data[batch, frame, N, 0]*scale_foot2meter, array_data[batch, frame, N, 1]*scale_foot2meter)
-                    writer.writerow(traj)
+        for frame in range(30):
+            for N in range(11):
+                traj = (batch*30+frame, batch*11+N, array_data[batch, frame, N, 0]*scale_foot2meter, array_data[batch, frame, N, 1]*scale_foot2meter)
+                writer.writerow(traj)
 
-
-    elif args.split == 'val':
-
-        for batch in range(num_batch):
-            
-            ## for val only:
-            if batch == 1000:
-                exit()
-
-            if batch % 100 == 0:
-                print(batch, '/', val_size)
-
-            for frame in range(30):
-                for N in range(11):                    
-                    traj = (batch*30+frame, batch*11+N, array_data[batch, frame, N, 0]*scale_foot2meter, array_data[batch, frame, N, 1]*scale_foot2meter)
-                    writer.writerow(traj)
-    
-    else:
-        for batch in range(num_batch):
-            
-            ## skip the validation part
-            if batch < val_size:
-                continue
-            
-            if batch % 100 == 0:
-                print(batch-val_size, '/', num_batch-val_size)
-                
-            for frame in range(30):
-                for N in range(11):                    
-                    traj = (batch*30+frame, batch*11+N, array_data[batch, frame, N, 0]*scale_foot2meter, array_data[batch, frame, N, 1]*scale_foot2meter)
-                    writer.writerow(traj) 
